7e0cb884e22f652d7a54410335e8bc79890610ef
rim0703 <rim0703@163.com>
2020-12-29 22:57:34 +0900

    아스키코드

---
+++ CLASS1/P11654.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+        char ch=str.charAt(0);
+        System.out.println((int)ch);
+    }
+}
