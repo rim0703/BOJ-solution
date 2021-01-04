73ad53c8f1c1fcf0b541f7f4870789d28d7c7250
rim0703 <rim0703@163.com>
2021-01-04 16:31:32 +0900

    이항 계수 1

--- CLASS2/P11050.java
+++ CLASS2/P11050.java
@@ -4,15 +4,14 @@ import java.io.InputStreamReader;
 
 public class P11050 {
     public static int sol(int a, int b){
-
         int N = 1,M = 1;
         for(int i=0;i<b;i++){
             N=N*(a-i);
             M=M*(i+1);
         }
-
         return N/M;
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String[] num=br.readLine().split(" ");
